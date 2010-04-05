ae454716cc872f530f51bebdd8f4d676b9edc5f2
Max Shinn <max@max-studio>
2010-04-05 14:15:55 -0500

    The stuff I forgot

---
+++ distractionFree.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'distractionfree.ui'
+#
+# Created: Tue Mar 23 14:54:31 2010
+#      by: PyQt4 UI code generator 4.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+class Ui_distractionFree(object):
+    def setupUi(self, distractionFree):
+        distractionFree.setObjectName("distractionFree")
+        distractionFree.resize(377, 315)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(distractionFree.sizePolicy().hasHeightForWidth())
+        distractionFree.setSizePolicy(sizePolicy)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(distractionFree)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
+        self.retranslateUi(distractionFree)
+        QtCore.QMetaObject.connectSlotsByName(distractionFree)
+
+    def retranslateUi(self, distractionFree):
+        distractionFree.setWindowTitle(QtGui.QApplication.translate("distractionFree", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+
